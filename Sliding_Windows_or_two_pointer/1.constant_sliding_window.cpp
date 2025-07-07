4f69fa4fb44b234b89cd56105362ed000bb3bb04
ritik7739 <ritik.kumar7739@gmail.com>
2025-07-07 17:26:40 +0530

    Update the constant window problem

--- Sliding_Windows_or_two_pointer/1.constant_sliding_window.cpp
+++ Sliding_Windows_or_two_pointer/1.constant_sliding_window.cpp
@@ -1,7 +1,5 @@
                             
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include<bits/stdc++.h>
 using namespace std;
 
 int maxConsecutiveSum(vector<int>& arr, int K) {
